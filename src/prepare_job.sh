1341d06e60f9df8129405a93bb4552817df0695b
PrometheusPi <r.pausch@hzdr.de>
2014-01-27 15:48:26 +0100

    add comments to prepare_job.sh

--- src/prepare_job.sh
+++ src/prepare_job.sh
@@ -19,31 +19,44 @@
 # If not, see <http://www.gnu.org/licenses/>.
 #
 
+
+
+#---------------------------------------------------------------
+# INFO:
+# Running this shell script generates a submit file either
+# for a MPI parallelsation or a PBS-Array-Job parallelsation.
+#---------------------------------------------------------------
+
+
+
 JOBNAME=MPI_lib1
 SUBMITFILE=submit.sh
 QUEUE=laser
 
 WALLTIME=02:00:00
 
-#Jobstructure:
-#for MPI:
+# Jobstructure:
+# for MPI:
 NUMBERNODES=4
 NUMBERCORES=32
-#for Array Jobs:
+# for Array Jobs:
 ARRAYMAX=$(echo $NUMBERNODES " * " $NUMBERCORES " - 1 " | bc )
 
+# User interaction required
+# present options:
 echo "for MPI enter 1"
-echo "for Array jobs enter 2"
+echo "for PBS-Array jobs enter 2"
 
+# read chosen parallelization method
 read DECISION
 case  $DECISION in
     1) echo "MPI choosen";;
-    2) echo "Array jobs choosen";;
+    2) echo "PBS-Array jobs choosen";;
     *) echo "none choosen"
        exit 1 ;;
 esac
 
-
+# generate submit file (the same for MPI and PBS-array jobs)
 echo "#PBS -q "$QUEUE > $SUBMITFILE
 echo "#PBS -l walltime="$WALLTIME >> $SUBMITFILE
 echo "#PBS -N "$JOBNAME >> $SUBMITFILE
@@ -53,28 +66,34 @@ echo "#PBS -e ./error.txt" >> $SUBMITFILE
 echo "#PBS -o ./output.txt" >> $SUBMITFILE
 echo  "" >> $SUBMITFILE
 
+# in case of a MPI parallelisation:
 if [ $DECISION -eq 1 ]
 then
     echo "#PBS -l nodes="$NUMBERNODES":ppn="$NUMBERCORES >> $SUBMITFILE
 else
+    # in case of a PBS-array parallelisation
     echo "#PBS -l nodes=1:ppn=1" >> $SUBMITFILE
     echo "#PBS -t 0-"$ARRAYMAX >> $SUBMITFILE
     echo "ARRAYMAX="$ARRAYMAX >> $SUBMITFILE
     echo "export ARRAYMAX" >> $SUBMITFILE
 fi
 
+# add newline to submit file
 echo  "" >> $SUBMITFILE
 
+# in case of MPI parallelisation
 if [ $DECISION -eq 1 ]
 then
     echo -n "mpiexec --display-map -mca plm rsh -mca btl openib,self,sm -n " >> $SUBMITFILE
     echo  $(echo $NUMBERNODES " * " $NUMBERCORES | bc )" ./executable" >> $SUBMITFILE
     make MPI
 else
+    # in case of PBS-array paparllelisation
     echo  " ./executable" >> $SUBMITFILE    
     make ARRAY
 fi
 
+# add newline to submit file
 echo  "" >> $SUBMITFILE
 
 
